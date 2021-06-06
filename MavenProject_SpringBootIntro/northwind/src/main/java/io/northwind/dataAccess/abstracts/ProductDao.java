da83cc283876f5c3b267b1a35e57b416ac88fe38
merdemli <57098477+merdemli@users.noreply.github.com>
2021-06-06 10:34:01 +0300

    Camp Project

--- MavenProject_SpringBootIntro/northwind/src/main/java/io/northwind/dataAccess/abstracts/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package io.northwind.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import io.northwind.entities.concretes.Product;
-
-public interface ProductDao extends JpaRepository<Product,Integer>{   
-
-}
