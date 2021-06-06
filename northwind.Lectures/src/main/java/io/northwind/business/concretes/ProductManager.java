9937b0ad03dc3986ee6aec71ff118ec9025dcab7
merdemli <57098477+merdemli@users.noreply.github.com>
2021-06-06 09:49:43 +0300

    Revert "commit"
    This reverts commit 1330b9b3155722e8ed515fb05cb4ab02917b4e3c.

---
+++ northwind.Lectures/src/main/java/io/northwind/business/concretes/ProductManager.java
@@ -0,0 +1,124 @@
+ package io.northwind.business.concretes;
+
+
+import java.util.List;
+		   //bean:proje class'ı//instance verir 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import io.northwind.business.abstracts.ProductService;
+import io.northwind.core.utilities.results.DataResult;
+import io.northwind.core.utilities.results.Result;
+import io.northwind.core.utilities.results.SuccessDataResult;
+import io.northwind.core.utilities.results.SuccessResult;
+import io.northwind.dataAccess.abstracts.ProductDao;
+import io.northwind.entities.concretes.Product;
+import io.northwind.entities.dtos.ProductWithCategoryDto;
+
+
+@Service //bu class servis görevi görecek demektir 
+public class ProductManager implements ProductService{
+
+	private ProductDao productDao;
+	
+	                  //Spring'den gelir ProductDao'nun instance'ı olabilcek sınıfı üretir buraya verir
+				     //ProductDao'nun ne oldugunu benim sana vereblmem için üzerinne bir autowired eklenir
+	@Autowired   
+	public ProductManager(ProductDao prodactDao) {
+		super();
+		this.productDao = prodactDao;
+	}
+	
+	
+	@Override
+	public DataResult<List<Product>> getAll() {
+		
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(),"Data Listelendi");
+				//DataResult; bir entity'nin dönüşünü anlatır.
+				 
+	}							//JPA repository'den gelir
+
+
+	@Override
+	public Result add(Product product) {
+		this.productDao.save(product);
+		return new SuccessResult("Ürün eklendi");//data dönmediğiimiz için direkt SuccessResult'la mesaj döneriz
+	}
+
+
+	@Override
+	public DataResult<Product> getByProductName(String productName) {
+		return new SuccessDataResult<Product>(this.productDao.getByProductName(productName),"Data Listelendi"); 
+	}
+
+
+	@Override
+	public DataResult<Product> getByProductNameAndCategoryId(String productName, int categoryId) {
+
+		return new SuccessDataResult<Product>(this.productDao.getByProductNameAndCategory_CategoryId(productName, categoryId),
+				"Data listelendi");
+	}
+
+
+	@Override
+	public DataResult<List<Product>> getByProductNameOrCategoryId(String productName, int categoryId) {
+		
+		return new SuccessDataResult<List<Product>>(this.productDao.getByProductNameOrCategory_CategoryId(productName, categoryId));
+	}
+
+
+	@Override
+	public DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories) {
+		
+		return new SuccessDataResult<List<Product>>(this.productDao.getByCategoryIn(categories), "data listelendi");
+	}
+
+
+	@Override
+	public DataResult<List<Product>> getByProductNameContains(String productName) {
+		//business codes,eğer bundan geçmezse daoya gitmez
+		return new SuccessDataResult<List<Product>>(this.productDao.getByProductNameContains(productName), "data listelendi");
+	}
+
+
+	@Override
+	public DataResult<List<Product>> getByProductNameStartsWith(String productName) {
+		
+		return new SuccessDataResult<List<Product>>(this.productDao.getByProductNameStartsWith(productName), "data listelendi");
+	}
+
+
+	@Override
+	public DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId) {
+		
+		return new SuccessDataResult<List<Product>>(this.productDao.getByNameAndCategory(productName, categoryId), "");
+	}
+
+
+	@Override
+	public DataResult<List<Product>> getAll(int pageNo, int pageSize) {  //Sayfalama
+		
+		Pageable pageable = PageRequest.of(pageNo-1, pageSize); //pageable nesnesi oluşturulur
+		
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(pageable).getContent(), "basarılı");
+	}
+
+
+	@Override
+	public DataResult<List<Product>> getAllSorted() {
+		
+		Sort sort = Sort.by(Sort.Direction.ASC, "productName");   //Sort nesnesi oluşturulur//hangi yönde sıralayacagını sööyleriz
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(sort), "Başarılı");
+	}
+
+
+	@Override
+	public DataResult<List<ProductWithCategoryDto>> getProductWithCategoryDetails() {
+		
+		return new SuccessDataResult<List<ProductWithCategoryDto>>(this.productDao.getProductWithCategoryDetails(),"Data listelendi");
+	}
+
+}//arayüzün bana verdiği parametreleri dao'ya gönderiyorum
