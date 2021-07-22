a207bb37a07c45e8fd7897f8028f93e41c854829
merdemli <57098477+merdemli@users.noreply.github.com>
2021-07-22 03:50:24 +0300

    commit

--- JS_Exercises/ex16_prototype.js
+++ JS_Exercises/ex16_prototype.js
@@ -1,6 +1,34 @@
 //ES ile beraber artık class yapısı da kullanılabilir oldu
 
 class Customer
-{
-    constructor(id,customerNumber)
+{         //patern
+    constructor(id,customerNumber){
+        this.id=id;                       //this : Customer class'ının customer1 instancee'ına karşılık gelir 
+        this.customerNumber = customerNumber;   //aslında burada daprototyping yaptık
+        
+    } 
+} 
+
+let customer1 = new Customer(1, "15287");
+customer1.name = "Evrim Erdemli" //instance 'a prototyping
+console.log(customer1.name);
+
+Customer.x= "X"; //Class'a prototype yapıldı
+console.log(Customer.x) 
+
+console.log(customer1.customerNumber)
+
+
+class IndividualCustomer extends Customer{
+    constructor(firstName,id,customerNumber){
+    super(id,customerNumber)
+    this.firstName =firstName;
+    }
+}
+
+class CoorparateCustomer extends Customer{
+    constructor(companyName,id,customerNumber){
+    super(id,customerNumber)
+    this.companyName =companyName;
+    }
 }
